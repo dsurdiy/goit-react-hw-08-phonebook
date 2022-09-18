3b22dae60ee244ce454ab543af9ca0088b8aa3b7
DimaSurdiy <dsurdiy@ukr.net>
2022-09-18 12:34:00 +0300

    update App and index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,77 +1,16 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { FcContacts } from 'react-icons/fc';
-import { FallingLines } from 'react-loader-spinner';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ContactForm } from 'components/ContactForm/ContactForm';
-import { ContactList } from 'components/ContactList/ContactList';
-import { Filter } from 'components/Filter/Filter';
-import { Container, AppTitle, ContactsTitle } from './App.styled';
-import {
-  useGetContactsQuery,
-  useAddContactMutation,
-} from 'redux/contactsSlice';
-import { changeFilter } from 'redux/filterSlice';
+import { Routes, Route } from 'react-router-dom';
+
+import { Layout } from 'components/Layout/Layout';
+import { ContactsPage } from 'pages/ContactsPage';
+import { RegisterPage } from 'pages/RegisterPage';
 
 export const App = () => {
-  const { data: contacts, isLoading } = useGetContactsQuery();
-  const filter = useSelector(state => state.filter);
-  const [addContact] = useAddContactMutation();
-  const dispatch = useDispatch();
-
-  const formSubmitHandler = async newContact => {
-    const normalizedNewContactsName = newContact.name.toLowerCase();
-    const existingСontact = contacts.find(
-      ({ name }) => name.toLowerCase() === normalizedNewContactsName
-    );
-
-    if (existingСontact) {
-      return toast.warn(`${newContact.name} is already in contacts`);
-    }
-
-    try {
-      await addContact(newContact);
-      toast.success(`${newContact.name} successfully added :)`);
-    } catch (error) {
-      toast.error('Something went wrong :(');
-    }
-  };
-
-  const getFilteredContacts = () => {
-    const normalizedFilter = filter.toLowerCase().trim();
-
-    return contacts?.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  const handleFilterChange = e => {
-    const newFilter = e.target.value;
-    dispatch(changeFilter(newFilter));
-  };
-
-  const isContacts = getFilteredContacts()?.length !== 0;
-
   return (
-    <Container>
-      <AppTitle>
-        Phonebook <FcContacts size={30} />
-      </AppTitle>
-      <ContactForm onSubmit={formSubmitHandler} />
-      <ContactsTitle>Contacts</ContactsTitle>
-      <Filter value={filter} onChange={handleFilterChange} />
-      {isLoading ? (
-        <FallingLines
-          color="#ff7043"
-          width="150"
-          visible={true}
-          ariaLabel="falling-lines-loading"
-        />
-      ) : (
-        <ContactList contacts={getFilteredContacts()} />
-      )}
-      {!isContacts && <b>There are no contacts...</b>}
-      <ToastContainer position="top-center" />
-    </Container>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 };

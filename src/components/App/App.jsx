d71565e00441bb0c08fd20c368190eaca2124355
DimaSurdiy <dsurdiy@ukr.net>
2022-09-19 21:40:05 +0300

    update index.js and App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import { Layout } from 'components/Layout/Layout';
 import { ContactsPage } from 'pages/ContactsPage';
 import { RegisterPage } from 'pages/RegisterPage';
+import { LoginPage } from 'pages/LoginPage';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="contacts" element={<ContactsPage />} />
-        <Route path="register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="contacts" element={<ContactsPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
+      </Routes>
+      <ToastContainer position="top-right" />
+    </>
   );
 };

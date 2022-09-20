3090d18485ed93c158d3dc26decfcdaf2cff247c
DimaSurdiy <dsurdiy@ukr.net>
2022-09-20 15:50:55 +0300

    add homepage to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import { Layout } from 'components/Layout/Layout';
+import { HomePage } from 'pages/HomePage';
 import { ContactsPage } from 'pages/ContactsPage';
 import { RegisterPage } from 'pages/RegisterPage';
 import { LoginPage } from 'pages/LoginPage';
@@ -11,6 +12,7 @@ export const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
           <Route path="contacts" element={<ContactsPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />

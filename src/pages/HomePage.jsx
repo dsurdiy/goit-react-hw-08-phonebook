16a5649ff9318cb5ffd0660d5aa8dd878ebda8ee
DimaSurdiy <dsurdiy@ukr.net>
2022-09-20 15:50:22 +0300

    HomePage added

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import { MdOutlineContacts } from 'react-icons/md';
+
+export const HomePage = () => {
+  return (
+    <main
+      style={{
+        margin: '50px auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '20px',
+        width: '600px',
+        padding: '20px',
+        boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+        borderRadius: '10px',
+      }}
+    >
+      <p>
+        <MdOutlineContacts size={70} />
+      </p>
+      <h1>Welcome to Phonebook App</h1>
+    </main>
+  );
+};

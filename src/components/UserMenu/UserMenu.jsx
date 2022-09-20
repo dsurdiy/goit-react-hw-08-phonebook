fea2880a6ca59e3aa509b27b09a660e1c4ceb762
DimaSurdiy <dsurdiy@ukr.net>
2022-09-20 15:50:41 +0300

    UserMenu updated

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,17 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import authOperations from 'redux/auth/auth-operations';
 import authSelectors from 'redux/auth/auth-selectors';
 
 export const UserMenu = () => {
+  const dispatch = useDispatch();
   const email = useSelector(authSelectors.getEmail);
 
   return (
     <div>
       <span>{email}</span>
-      <button type="button">Logout</button>
+      <button type="button" onClick={() => dispatch(authOperations.logout())}>
+        Logout
+      </button>
     </div>
   );
 };

6981bfe42657571011ba2939a5e120fc884178d8
DimaSurdiy <dsurdiy@ukr.net>
2022-09-21 21:38:40 +0300

    add logout and getCurrentUser operations

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -38,8 +38,36 @@ const login = createAsyncThunk(
   }
 );
 
+const logout = createAsyncThunk('auth/logout', async () => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const getCurrentUser = createAsyncThunk('auth/refresh', async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const persistedToken = state.auth.token;
+
+  if (persistedToken === null) {
+    return thunkAPI.rejectWithValue();
+  }
+
+  token.set(persistedToken);
+  try {
+    const { data } = await axios.get('/users/current');
+    return data;
+  } catch ({ response }) {
+    return thunkAPI.rejectWithValue(response.status);
+  }
+});
+
 const operations = {
   register,
   login,
+  logout,
+  getCurrentUser,
 };
 export default operations;

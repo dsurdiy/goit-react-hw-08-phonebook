a550cc9a49515da398c576706cbc120b8b280e32
DimaSurdiy <dsurdiy@ukr.net>
2022-09-23 22:11:58 +0300

    add user routes, update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-
+import { UserRoutes } from 'UserRoutes';
 import { Layout } from 'components/Layout/Layout';
-import { HomePage } from 'pages/HomePage';
-import { ContactsPage } from 'pages/ContactsPage';
-import { RegisterPage } from 'pages/RegisterPage';
-import { LoginPage } from 'pages/LoginPage';
 
 import authOperations from 'redux/auth/auth-operations';
 import authSelectors from 'redux/auth/auth-selectors';
-import PrivateRoute from 'components/PrivateRoute';
-import PublicRoute from 'components/PublicRoute';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -27,30 +20,8 @@ export const App = () => {
   return (
     !isGettingCurrentUser && (
       <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="contacts" element={<ContactsPage />} />
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="login" element={<LoginPage />} />
-          </Route>
-        </Routes>
-
-        {/* <PublicRoute path="/">
-        <HomePage />
-      </PublicRoute>
-
-      <PublicRoute path="/register" restricted>
-        <RegisterPage />
-      </PublicRoute>
-
-      <PublicRoute path="/login" redirectTo="/contacts" restricted>
-        <LoginPage />
-      </PublicRoute>
-
-      <PrivateRoute path="/contacts" redirectTo="/login">
-        <ContactsPage />
-      </PrivateRoute> */}
+        <Layout />
+        <UserRoutes />
 
         <ToastContainer position="top-right" />
       </>

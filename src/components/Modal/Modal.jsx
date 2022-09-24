08a24491ae7dcc0fb1d08188f7aeee906888bcf8
DimaSurdiy <dsurdiy@ukr.net>
2022-09-24 23:13:09 +0300

    update Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,33 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, Content } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export const Modal = ({ children }) => {
+export const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  const handleOverlayClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
   return createPortal(
-    <Overlay>
+    <Overlay onClick={handleOverlayClick}>
       <Content>{children}</Content>
     </Overlay>,
     modalRoot
